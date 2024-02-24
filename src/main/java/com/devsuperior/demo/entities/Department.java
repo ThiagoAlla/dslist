416c1160801bdfd84f426e1dc170a29aa3cc6c38
Thiago Allan <thiagoallan864@gmail.com>
2024-02-24 18:45:00 -0300

    project created

---
+++ src/main/java/com/devsuperior/demo/entities/Department.java
@@ -0,0 +1,23 @@
+package com.devsuperior.demo.entities;
+
+public class Department {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+
+    }
+}
